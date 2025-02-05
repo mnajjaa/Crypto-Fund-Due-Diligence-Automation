bc3d78b504afbdb4b61a525bbc12cbc94ee5afed
mnajjaa <ibtihel.mnaja@esprit.tn>
2025-02-05 18:43:21 +0100

    Postgres

--- DueDiligence/urls.py
+++ DueDiligence/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Core.urls')),  
+    path('', include('Core.urls')),  
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
